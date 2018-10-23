cc8a13c9888a7933c24bd2d47638be3095ecdd1e
medmene <38806186+medmene@users.noreply.github.com>
2018-10-23 11:19:28 +0700

    Update Form1.cs

--- NetworkParall/Form1.cs
+++ NetworkParall/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,7 +21,6 @@ namespace NetworkParall
         string filepath = "";
         string UserSrcCode = "";
         Socket sListener;
-        Socket sender;
         public Form1()
         {
             InitializeComponent();
@@ -29,35 +28,26 @@ namespace NetworkParall
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (UserSrcCode != ""){
+            if (UserSrcCode != "")
+            {
                 // Устанавливаем для сокета локальную конечную точку
                 IPHostEntry ipHost = Dns.GetHostEntry("localhost");
                 IPAddress ipAddr = ipHost.AddressList[0];
                 IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+                progressBar1.Enabled = true;
+                progressBar2.Enabled = true;
+                progressBar3.Enabled = true;
 
                 // Создаем сокет Tcp/Ip
                 sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                
-#if false
-                // Соединяем сокет с удаленной точкой
-                sListener.Connect(ipEndPoint);
 
-                richTextBox1.Text += "Сокет соединяется с "+ sListener.RemoteEndPoint.ToString();
-                byte[] msg = Encoding.UTF8.GetBytes(UserSrcCode);
-                // Отправляем данные через сокет
-                int bytesSent = sListener.Send(msg);
-
-                // Получаем ответ от сервера
-                int bytesRec = sListener.Receive(bytes);
-
-                richTextBox1.Text += "\nОтвет от сервера: "+Encoding.UTF8.GetString(bytes, 0, bytesRec) + "\n\n";
-#endif
-            // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
-            try
+                // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+                try
                 {
                     sListener.Bind(ipEndPoint);
                     sListener.Listen(10);
-
+                    int someInt = 0; //
+                    int curUser = 1; //выделение номера пользователю
                     // Начинаем слушать соединения
                     while (true)
                     {
@@ -72,26 +62,70 @@ namespace NetworkParall
                         int bytesRec = handler.Receive(bytes);
 
                         data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
-                        if(data == "dvc_firstConn")
+                        /////////////////////////////////////////////////////
+                        /////////////////////////////////////////////////////
+                        /////////////////////////////////////////////////////
+                        //первое присоединение
+                        if (data == "dvc_firstConn")
                         {
-                            // Отправляем ответ клиенту
-                            byte[] msg = Encoding.UTF8.GetBytes(UserSrcCode);
+                            byte[] msg = Encoding.UTF8.GetBytes(curUser.ToString() + UserSrcCode);
+                            curUser++;
                             handler.Send(msg);
                         }
-
-                        if (data.IndexOf("<TheEnd>") > -1)
+                        //отправлять результат
+                        else if (data == "sndAnsw")
                         {
-                            richTextBox1.Text += "Сервер завершил соединение с клиентом.";
-                            break;
+                            if (checkBox1.Checked == true)
+                            {
+                                byte[] msg = Encoding.UTF8.GetBytes("sndRes");
+                                handler.Send(msg);
+                            }
+                            else
+                            {
+                                byte[] msg = Encoding.UTF8.GetBytes("DontSndRes");
+                                handler.Send(msg);
+                            }
+                        }
+                        //количество посылок
+                        else if (data == "CountPrc")
+                        {
+                            if (textBox1.Text != "" && Int32.TryParse(textBox1.Text, out someInt))
+                            {
+                                byte[] msg = Encoding.UTF8.GetBytes(textBox1.Text);
+                                handler.Send(msg);
+                            }
+                            else
+                            {
+                                byte[] msg = Encoding.UTF8.GetBytes("100000");
+                                handler.Send(msg);
+                            }
+                        }
+                        //процент выполнения
+                        else {
+                            string[] ss = data.Split('_');
+                            if (Int32.TryParse(ss[0], out someInt))
+                            {
+                                if (ss[1] == "2") progressBar1.Value = someInt;
+                                if (ss[1] == "3") progressBar2.Value = someInt;
+                                if (ss[1] == "4") progressBar3.Value = someInt;
+                                byte[] msg = Encoding.UTF8.GetBytes("good");
+                                handler.Send(msg);
+                            }
+                            //отключение
+                            else if (data.IndexOf("<TheEnd>") > -1)
+                            {
+                                richTextBox1.Text += "Сервер завершил соединение с клиентом." + data[data.Length - 1];
+                                break;
+                            }
                         }
 
                         handler.Shutdown(SocketShutdown.Both);
                         handler.Close();
                     }
                 }
-                catch (Exception ex) {richTextBox1.Text += (ex.ToString());}
-
+                catch (Exception ex) { richTextBox1.Text += (ex.ToString()); }
             }
+            else richTextBox1.Text += "User code is empty!\n";
         }
 
         private void button2_Click(object sender, EventArgs e){
